452625625914b2d58b4a4e1414a22f4084ae5c3e
David Chen <david.c@cherry.tv>
2024-12-14 01:03:57 +0800

    fixed: cors issue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  // NOTE later should be configured as default
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
