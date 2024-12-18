1aa35e7bfbf00cf3147dfaf446415d1dc990599a
David Chen <david.c@cherry.tv>
2024-12-19 00:29:55 +0800

    fix: shadcn background issue revert: tailwin.confg.ts to next just applied with shadcn

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,8 +2,11 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx,css}"],
-  safelist: ["dark"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
